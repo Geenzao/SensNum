ff4cfd5b2db89ba8f0eb199f635f6e0fd8bed902
Geenzao <quentinlorenzo04@gmail.com>
2025-01-23 09:56:26 +0100

    Tout marche bien ensemble

--- Assets/Scripts/DebloqueurDePassage.cs
+++ Assets/Scripts/DebloqueurDePassage.cs
@@ -4,6 +4,7 @@ using static PathManager;
 public class DebloqueurDePassage : MonoBehaviour
 {
     public GameObject passage1;
+    public GameObject eauBoueuse;
 
     // Update is called once per frame
     void Update()
@@ -14,6 +15,7 @@ public class DebloqueurDePassage : MonoBehaviour
                 break;
             case PathState.Mine:
                 passage1.SetActive(false);
+                eauBoueuse.SetActive(true);
                 break;
         }
 
